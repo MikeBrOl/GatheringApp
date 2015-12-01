7eb6c8b011dc00e68d8d42415baf7da275ba469d
Urkosh <uncleurkosh@gmail.com>
2015-12-01 11:26:25 +0100

    Created dummy classes

---
+++ app/src/main/java/janco/gatheringapp/Database/DummyClassDB.java
@@ -0,0 +1,7 @@
+package janco.gatheringapp.Database;
+
+/**
+ * Created by Mads on 01-12-2015.
+ */
+public class DummyClassDB {
+}
---
+++ app/src/main/java/janco/gatheringapp/Model/DummyClassModel.java
@@ -0,0 +1,7 @@
+package janco.gatheringapp.Model;
+
+/**
+ * Created by Mads on 01-12-2015.
+ */
+public class DummyClassModel {
+}
