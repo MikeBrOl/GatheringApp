191892e8c906020808de05798ad8369e0535c3a5
Urkosh <uncleurkosh@gmail.com>
2015-12-01 11:23:30 +0100

    Created folders

--- app/src/main/java/janco/gatheringapp/Activities/MainActivity.java
+++ app/src/main/java/janco/gatheringapp/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package janco.gatheringapp;
+package janco.gatheringapp.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import janco.gatheringapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
