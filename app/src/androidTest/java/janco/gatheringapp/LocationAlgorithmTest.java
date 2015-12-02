ff954e1fb7b75394f5abeb00abc2565ec07c6a37
Urkosh <uncleurkosh@gmail.com>
2015-12-02 13:55:18 +0100

    updated algorithm and it's test

--- app/src/androidTest/java/janco/gatheringapp/LocationAlgorithmTest.java
+++ app/src/androidTest/java/janco/gatheringapp/LocationAlgorithmTest.java
@@ -1,42 +1,46 @@
-//package janco.gatheringapp;
-//
-//import android.test.AndroidTestCase;
-//
-//import junit.framework.Assert;
-//
-//import java.util.ArrayList;
-//
-//import janco.gatheringapp.Activities.LocationAlgorithm;
-//
-///**
-// * Created by Mads on 02-12-2015.
-// */
-//public class LocationAlgorithmTest extends AndroidTestCase
-//{
-//    private LocationAlgorithm algorithm;
-//    public LocationAlgorithmTest(){
-//        this.algorithm = new LocationAlgorithm();
-//    }
-//
-//    public void testDependencies()
-//    {
-//        assertNotNull(algorithm);
-//    }
-//
-//    public void testLocationAlgorithmForNotices()
-//    {
-//        GeoPoint mockLocation = new GeoPoint();
-//        double mockRadius = 2000;
-//        ArrayList<Notice> testList = algorithm.boundingBoxCalculation(0, mockLocation, mockRadius);
-//        Assert.assertNotNull(testList);
-//    }
-//
-//    public void testLocationAlgorithmForUsers()
-//    {
-//        GeoPoint mockLocation = new GeoPoint();
-//        double mockRadius = 2000;
-//        ArrayList<User> testList = algorithm.boundingBoxCalculation(1, mockLocation, mockRadius);
-//        Assert.assertNotNull(testList);
-//    }
-//
-//}
+package janco.gatheringapp;
+
+import android.test.AndroidTestCase;
+
+import junit.framework.Assert;
+
+import com.google.android.maps.GeoPoint;
+import java.util.ArrayList;
+
+import janco.gatheringapp.Activities.LocationAlgorithm;
+import janco.gatheringapp.Model.User;
+
+/**
+ * Created by Mads on 02-12-2015.
+ */
+public class LocationAlgorithmTest extends AndroidTestCase
+{
+    private LocationAlgorithm algorithm;
+    public LocationAlgorithmTest(){
+        this.algorithm = new LocationAlgorithm();
+    }
+
+    public void testDependencies()
+    {
+        assertNotNull(algorithm);
+    }
+
+    public void testLocationAlgorithmForNotices()
+    {
+        //TODO create date and status objects
+        GeoPoint mockLocation = new GeoPoint(55, 55);
+        double mockRadius = 2000;
+        ArrayList<Notice> testList = algorithm.boundingBoxCalculation(0, mockLocation, mockRadius, date, status);
+        Assert.assertNotNull(testList);
+    }
+
+    public void testLocationAlgorithmForUsers()
+    {
+        //TODO create date and status objects
+        GeoPoint mockLocation = new GeoPoint(55,55);
+        double mockRadius = 2000;
+        ArrayList<User> testList = algorithm.boundingBoxCalculation(1, mockLocation, mockRadius, date, status);
+        Assert.assertNotNull(testList);
+    }
+
+}
