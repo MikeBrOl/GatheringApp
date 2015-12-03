81b6d91dd046525c43e57392bfaa16679ecbdaab
Kim Bach <kimbach@hotmail.com>
2015-12-03 12:14:25 +0100

    encrypting password in CreateNewUseractivity

--- app/src/main/java/janco/gatheringapp/Activities/LocationAlgorithm.java
+++ app/src/main/java/janco/gatheringapp/Activities/LocationAlgorithm.java
@@ -23,12 +23,14 @@ public class LocationAlgorithm
     }
 
     /**
-     * A simple variant of the bounding box algorithm, using a radius and date object to determine what notices are within range and still active
+     * A simple variant of the bounding box algorithm, using a radius and date object
+     * to determine what notices are within range and still active
      * @param latitude is a double
      * @param longitude is a double
      * @param radius is a simple int, unit of meassure meters
      * @param date a date object
-     * @return ArrayList<Notice> with all available notices within the given radius based of the latitude and longitude of the current position
+     * @return ArrayList<Notice> with all available notices within the given radius based of the
+     *         latitude and longitude of the current position
      */
     public ArrayList<Notice> boundingBoxCalculationForNotice(double latitude, double longitude, double radius, Date date)
     {
@@ -41,8 +43,8 @@ public class LocationAlgorithm
         double lonMax;
 
 
-        latVar = radius/31;
-        lonVar = radius/23.72;
+        latVar = radius/31;     // 31 meters per latitude second
+        lonVar = radius/23.72;  // 23.72 meters per longitude second
         latMin = latitude-latVar;
         latMax = latitude+latVar;
         lonMin = longitude-lonVar;
@@ -54,12 +56,14 @@ public class LocationAlgorithm
     }
 
     /**
-     *
+     * A simple variant of the bounding box algorithm, using a radius and user's status
+     * to determine what users are within range and still active
      * @param latitude is a double
      * @param longitude is a double
      * @param radius is an int, unit of meassurement is meters
      * @param status is a boolean, used to decide wether or not someone wants to find another person within range
-     * @return ArrayList<User> with all the available users within the given radius based of the latitude and longitude of the current position
+     * @return ArrayList<User> with all the available users within the given radius based of the
+     *         latitude and longitude of the current position
      */
     public ArrayList<User> boundingBoxCalculationForUsers(double latitude, double longitude, double radius, boolean status)
     {
