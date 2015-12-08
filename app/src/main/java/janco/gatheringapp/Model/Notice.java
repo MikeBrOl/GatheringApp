0a68903cc9516958dd6d44b36d840c5e82028840
Kim Bach <kimbach@hotmail.com>
2015-12-08 11:20:34 +0100

    Notice and NoticeModelTest

--- app/src/main/java/janco/gatheringapp/Model/Notice.java
+++ app/src/main/java/janco/gatheringapp/Model/Notice.java
@@ -1,7 +1,87 @@
 package janco.gatheringapp.Model;
 
+import java.sql.Date;
+
 /**
  * Created by Mads on 01-12-2015.
  */
-public class Notice {
+public class Notice
+{
+    String name;
+    String description;
+    String address;
+    Date time;
+    float latitude;
+    float longitude;
+
+    public Notice(String name, String description, String address, Date time,
+                  float latitude, float longitude)
+    {
+        this.name = name;
+        this.description = description;
+        this.address = address;
+        this.time = time;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Notice{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", address='" + address + '\'' +
+                ", time=" + time +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
 }
