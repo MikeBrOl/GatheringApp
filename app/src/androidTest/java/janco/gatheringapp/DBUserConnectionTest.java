7992e8676f67f50c06e59c9a6ec51160b0b0f45e
Kim Bach <kimbach@hotmail.com>
2015-12-10 11:18:59 +0100

    DBUserConnection og test klassen

---
+++ app/src/androidTest/java/janco/gatheringapp/DBUserConnectionTest.java
@@ -0,0 +1,71 @@
+package janco.gatheringapp;
+
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+import junit.framework.Assert;
+
+import java.util.ArrayList;
+
+import janco.gatheringapp.Database.DBUser;
+import janco.gatheringapp.Database.DBUserConnection;
+import janco.gatheringapp.Model.User;
+import janco.gatheringapp.Model.UserConnection;
+
+
+/**
+ * Created by Torn on 10-12-2015.
+ */
+public class DBUserConnectionTest extends AndroidTestCase
+{
+    DBUserConnection dbUserConnection;
+    DBUser dbUser;
+
+
+
+
+
+    public DBUserConnectionTest() {
+        dbUserConnection = new DBUserConnection();
+
+        dbUser = new DBUser();
+    }
+
+    public void testDependencies()
+    {
+        Assert.assertNotNull(dbUserConnection);
+    }
+
+    //
+    public void testInsertUserConnection()
+    {
+        User appUser = dbUser.getUserByUsername("test Username");
+        User connectedUser = dbUser.getUserByUsername("Insert Test");
+
+        UserConnection testUserConnection = new UserConnection(appUser, connectedUser);
+
+        int success = dbUserConnection.insertUserConnection(testUserConnection);
+
+        Assert.assertEquals(1, success );
+
+    }
+
+    public void testDeleteUserConnection()
+    {
+        User appUser = dbUser.getUserByUsername("test Username");
+        User connectedUser = dbUser.getUserByUsername("Insert Test");
+
+        UserConnection testUserConnection = new UserConnection(appUser, connectedUser);
+
+        int success = dbUserConnection.deleteUserConnection(testUserConnection);
+        Assert.assertEquals(1, success );
+    }
+
+    public void testGetUserConnectionsByAppUser()
+    {
+        User appUser = dbUser.getUserByUsername("test Username");
+        ArrayList<UserConnection> appUserUserConnections= dbUserConnection.getUserConnectionsByAppUser(appUser);
+
+        Assert.assertNotNull("Arraylist for found user connections: ", appUserUserConnections);
+    }
+}
---
+++ app/src/main/java/janco/gatheringapp/Database/DBUserConnection.java
@@ -0,0 +1,105 @@
+package janco.gatheringapp.Database;
+
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import janco.gatheringapp.Model.User;
+import janco.gatheringapp.Model.UserConnection;
+
+/**
+ * Created by Torn on 10-12-2015.
+ */
+public class DBUserConnection
+{
+    private DBConnection dbConnection;
+
+     public DBUserConnection()
+    {
+        this.dbConnection = new DBConnection();
+    }
+
+
+    public int insertUserConnection(UserConnection newUserConnection)
+    {
+        int check = 0;
+
+        try
+        {
+            Connection con = dbConnection.CONN();
+
+
+            User appUser = newUserConnection.getAppUser();
+            User connectedUser = newUserConnection.getConnectedUser();
+
+            // check if the user connection already exists as appUser
+            // equals appUser or connectedUser
+            // otherwise save the user connection in the database
+
+            // get the two users' IDs
+            int appUserID = appUser.getID();
+            int connectedUserID = connectedUser.getID();
+
+            Statement statement = con.createStatement();
+            String query = "IF NOT EXISTS (SELECT * FROM Connection " +
+                    "WHERE UserID1 = " + appUserID + " AND UserID2 = " + connectedUserID + " OR " +
+                    "WHERE UserID1 = " + connectedUserID + "AND UserID2 = " + appUserID + ")" +
+                    "INSERT INTO Connection " +
+                     "VALUES (" + appUserID + ","
+                      + connectedUserID + ");";
+
+                Log.e("Query: ", query);
+
+            check = statement.executeUpdate(query);;
+
+        }
+
+        catch (SQLException sqle)
+        {
+            sqle.getStackTrace();
+        }
+
+        return check;
+    }
+
+    public int deleteUserConnection(UserConnection userConnection)
+    {
+        int check = 0;
+
+
+
+        return check;
+    }
+
+    public ArrayList<UserConnection> getUserConnectionsByAppUser(User appUser)
+    {
+        ArrayList<UserConnection> foundUserConnections = null;
+
+
+
+        return foundUserConnections;
+    }
+
+//    private boolean checkForExistingUserConnection(User appUser, User connectedUser)
+//    {
+//        Connection con = dbConnection.CONN();
+//
+////        IF EXISTS (SELECT * FROM Table1 WHERE Column1='SomeValue')
+////        UPDATE Table1 SET (...) WHERE Column1='SomeValue'
+////        ELSE
+////        INSERT INTO Table1 VALUES (...)
+//
+//        // get users' IDS
+//        int appUserID = appUser.getID();
+//        int connectedUserID = connectedUser.getID();
+//
+//        Statement statement = con.prepareStatement()
+//
+//
+//        return false;
+//    }
+
+}
